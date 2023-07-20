6f0ea2ab06678e56406265e18224b2d240b51144
Kaleb-Nim <65989471+Kaleb-Nim@users.noreply.github.com>
2023-07-20 19:39:02 +0800

    Integrate a Streamlit APplication ( OOP the entire Langchain )
    Added my own custom wrapper class to setttle all the initialization of Pinecone, OpenAPI + Langchain.
    Down to one methods to call
    
    singlife.generateScript(Query,model_name, video_style )

---
+++ custom_llm/App.py
@@ -0,0 +1,104 @@
+import openai
+import streamlit as st
+from streamlit_chat import message
+from utils.singlife import Singlife
+
+# For displaying the output of print() in Streamlit
+from contextlib import contextmanager, redirect_stdout
+from io import StringIO
+
+
+# Setting page title and header
+st.set_page_config(page_title="AVA", page_icon=":robot_face:")
+st.markdown("<h1 style='text-align: center;'>AVA - a totally harmless chatbot 😬</h1>", unsafe_allow_html=True)
+
+# @st.cache_resource
+def init_singlife_class():
+    # Usually u just have to init with this line, werid streamlit thingy to cache 
+    singlife = Singlife()
+    return singlife
+
+singlife = init_singlife_class()
+
+@contextmanager
+def st_capture(output_func):
+    with StringIO() as stdout, redirect_stdout(stdout):
+        old_write = stdout.write
+
+        def new_write(string):
+            ret = old_write(string)
+            output_func(stdout.getvalue())
+            return ret
+
+        stdout.write = new_write
+        yield
+
+# Initialise session state variables
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+if 'model_name' not in st.session_state:
+    st.session_state['model_name'] = []
+
+if 'video_style' not in st.session_state:
+    st.session_state['video_style'] = ""
+
+
+# Sidebar - let user choose model, show total cost of current conversation, and let user clear the current conversation
+st.sidebar.title("Sidebar")
+model_name = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
+# Text input for user to enter the style of video script generated
+video_style = st.sidebar.text_input("Enter the style of video script you want to generate:", "Funny and sarcastic")
+counter_placeholder = st.sidebar.empty()
+clear_button = st.sidebar.button("Clear Conversation", key="clear")
+
+# Map model names to OpenAI model IDs
+if model_name == "GPT-3.5":
+    model = "gpt-3.5-turbo-0613"
+else:
+    model = "gpt-4-0613"
+
+# reset everything
+if clear_button:
+    st.session_state['generated'] = []
+    st.session_state['number_tokens'] = []
+    st.session_state['model_name'] = []
+
+
+# container for chat history
+response_container = st.container()
+# container for text box
+container = st.container()
+
+with container:
+    with st.form(key='my_form', clear_on_submit=True):
+        user_input = st.text_area("You:", key='input', height=100,value="I am travelling to Japan for a ski trip with my family next week.What kind of travel insurance coverage do we need?")
+        submit_button = st.form_submit_button(label='Send')
+
+    if submit_button and user_input:
+
+        with st.spinner(f"Running LLM"):
+            # st.write(f'🧾 Logs')
+            with st.expander(f"🧾 Logs"):
+                output = st.empty()
+                with st_capture(output.code):
+
+                    # This is how to use the class
+                    results = singlife.generateScript(query=user_input, model_name=model, video_style=video_style)
+
+        st.session_state['past'].append(user_input)
+        st.session_state['generated'].append(results)
+        st.session_state['model_name'].append(model_name)
+
+        # from https://openai.com/pricing#language-models
+
+if st.session_state['generated']:
+    with response_container:
+        for i in range(len(st.session_state['generated'])):
+            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state["generated"][i], key=str(i))
+            st.write(
+                f"Model used: {st.session_state['model_name'][i]}; Number of tokens: {st.session_state['total_tokens'][i]}; Cost: ${st.session_state['cost'][i]:.5f}")
+            counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
--- custom_llm/utils/singlife.py
+++ custom_llm/utils/singlife.py
@@ -1,8 +1,14 @@
+""" 
+Author: Kaleb Nim
+Date created: 2023-07-20
+"""
 import pinecone
 import openai
 import numpy as np
 import os
 from dotenv import load_dotenv
+# Import time 
+from time import time
 
 # Langchain
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -18,6 +24,7 @@ from langchain.prompts import (
     HumanMessagePromptTemplate,
 )
 from typing import Optional
+from langchain.chains import SimpleSequentialChain ,SequentialChain
 
 from langchain.chains.openai_functions import (
     create_openai_fn_chain,
@@ -35,7 +42,6 @@ print('PATH_TO_ENV: ', PATH_TO_ENV)
 class Singlife:
     """
     Main Wrapper class for Main Langchin
-
     Assumes the .env variables are in Sn33k directory
     """
 
@@ -79,3 +85,203 @@ class Singlife:
             print('vectorstore created succesfully')
         except Exception as e:
             raise Exception(f"Error creating vectorstore: {e}")
+        
+        # Default model used
+        self.gpt3_model = ChatOpenAI(model_name="gpt-3.5-turbo-0613", temperature=0)
+        self.gpt4_model = ChatOpenAI(model_name="gpt-4-0613", temperature=0)
+        self.current_model = self.gpt3_model
+
+
+    def generateScript(self, query: str, video_style: str = "Funny and sarcastic", model_name: str = 'gpt-3.5-turbo-0613'):
+        """
+        Main class method to generate video script from query
+
+        Args:
+            query (str): The user query to generate video script from
+            video_style (str): The video prompt style to use E.g. "Funny and Energetic", "Serious and Professional", "Calm and Friendly"
+            model_name (str): The model name to use for the LLMChain. Default is 'gpt-3.5-turbo-0613'
+        
+        Returns:
+        """
+        # Set the current model
+        # self._set_current_model(model_name=model_name)
+
+        # Set the video prompt style
+        self._set_video_prompt_style(video_style=video_style)
+
+        # Set the json schema
+        pass # TODO
+
+        # Initialize the overall chain
+        start_time = time()
+        self._overallChain_init()
+        print(f"Time to initialize overall chain: {start_time - time()}", flush=True)
+
+        # Run the overall chain
+        try:
+            result = self.overall_chain.run(query=query)
+        except Exception as e:
+            raise Exception(f"Error running overall_chain: {e}")
+        
+        return result
+
+    def _set_current_model(self, model_name: str):
+        """
+        Purpose:
+        Set the current model to use
+        """
+        if model_name == 'gpt-3.5-turbo-0613':
+            self.current_model = self.gpt3_model
+        elif model_name == 'gpt-4-0613':
+            self.current_model = self.gpt4_model
+        else:
+            raise Exception(f"Model name {model_name} not recognized, use 'gpt-3.5-turbo-0613' or 'gpt-4-0613'")
+        
+    def get_current_model(self):
+        """
+        Purpose:
+        Get the current model to use
+        """
+        return self.current_model
+    
+
+    def _qaChain_init(self):
+        """
+        Purpose:
+        Initialize the QA chain with the current model
+
+        Returns:
+            self.qa_chain to the initialized QA chain
+
+        Error handling:
+            Raises Exception if self.qa_chain cannot be initialized
+        """
+        # Create LLMChain
+        try:
+            qa_chain = RetrievalQA.from_chain_type(
+            llm=self.current_model,
+            chain_type="refine",
+            retriever=self.vectorstore.as_retriever(),
+            verbose=True,
+            )
+        except Exception as e:
+            raise Exception(f"Error creating qa_chain: {e}")
+        
+        self.qa_chain = qa_chain
+
+    def _videoChain_init(self):
+        """
+        Purpose:
+        Initialize the video chain with the current model, video_prompt, and json_schema
+
+        Returns:
+            self.video_chain to the initialized video chain logic
+
+        Error handling:
+            Raises Exception if self.video_chain cannot be initialized
+        """
+        
+        # Validate if self.video_prompt and self.json_schema are set
+        if self.video_prompt is None:
+            self._set_video_prompt_style()
+            # Raise warning that video_prompt_style is being set to default
+            print(f"Warning: video_prompt_style is being set to default: Funny and sarcastic", flush=True)
+
+        if self.json_schema is None:
+            self._set_json_schema()
+        
+        # Create video_chain
+        try:
+            self.video_chain = create_structured_output_chain(self.json_schema, self.current_model, self.video_prompt, verbose=True)
+        except Exception as e:
+            raise Exception(f"Error creating video_chain: {e}")
+    
+    def _overallChain_init(self):
+        """
+        Purpose:
+        Initialize the overall chain with the current model, video_prompt, and json_schema
+
+        Returns:
+            self.overall_chain to the initialized overall chain logic
+
+        Error handling:
+            Raises Exception if self.overall_chain cannot be initialized
+        """
+        # Create qa_chain (RetrievalQA) --> self.qa_chain
+        self._qaChain_init()
+        # Create video_chain --> self.video_chain
+        self._videoChain_init()
+        try:
+            self.overall_chain = SequentialChain(chains=[self.qa_chain, self.video_chain],input_variables=["query"])
+        except Exception as e:
+            raise Exception(f"Error creating overall_chain: {e}")
+
+    def _set_video_prompt_style(self, video_style="Funny and sarcastic"):
+        """
+        Purpose:
+        Set the video prompt style for the LLMChain, 
+        Default is "Funny and sarcastic"
+
+        Args:
+            video_style (str): The video prompt style to use E.g. "Funny and Energetic", "Serious and Professional", "Calm and Friendly"
+        
+        Returns:
+            self.video_prompt initialized to the video_style
+
+        Error handling:
+            Raises Exception video_prompt cannot be initialized
+        """
+        # Vaildate string input for video_style
+
+        try:
+            video_prompt = PromptTemplate(
+                template="""Goal:Generate 15-30sec video script based on custom knowledge base (Information below) and user query. Two components 1.Scene assets descriptions 2.Subtitle script 
+                Custom knowledge base:{result}\n\nUsing the above information, generate a video script that addresses this user query:\n\n"{query}".\nReturn the generated video script in the style/format: """+video_style,
+                input_variables= ["result", "query"]
+            )
+        except Exception as e:
+            raise Exception(f"Error creating video_prompt: {e}]\n\n Type video_prompt_style must be (str)")
+        
+        self.video_prompt= video_prompt
+
+    def _set_json_schema(self):
+        """
+        TODO - add flexibility to change the json schema
+        # Hard coded to one schema for now
+        Purpose:
+        Set the json schema for the LLMChain
+
+        Returns:
+            self.json_schema initialized to the json_schema
+        """
+
+        # Define the json schema
+        json_schema = {
+            "title": "generate_video_script",
+            "description": "Generates 15-30sec video script based on custom knowledge base. Two components 1.Scene descriptions 2.Subtitle script",
+            "type": "object",
+            "properties": {
+                "list_of_video_chunk": {
+                "type": "array",
+                "description": "List of video_chunk to be included in the video, one video chunk should last 3-5 seconds and is a dictionary with keys: 1. Scene  2. Subtitle",
+                "items": {
+                    "type": "object"
+                },
+                "properties": {
+                    "scene": {
+                    "type": "string",
+                    "description": "Scene description for video should be visual and general"
+                    },
+                    "subtitles": {
+                    "type": "string",
+                    "description": "Funny and sarcastic video subtitles script for video"
+                    }
+                },
+                "required": ["scene", "subtitles"]
+                }
+            },
+            "required": ["list_of_video_chunk"]
+        }
+
+        self.json_schema = json_schema
+
