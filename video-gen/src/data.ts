e9330ff09e6d7f3f6d24037912b5dc2a9d209086
Elliott Chong <elliottchong16@gmail.com>
2023-07-26 20:58:21 +0800

    meow

--- video-gen/src/data.ts
+++ video-gen/src/data.ts
@@ -1,70 +1,10 @@
-export const formatted = [
-	{
-		scene: 'diving in the ocean',
-		subtitles: [
-			'Are you ready for an unforgettable family ski trip to Japan?',
-			"Ensure your adventure is worry-free with Singlife's travel insurance!",
-		],
-	},
-	{
-		scene: 'Close-up of the Singlife travel insurance policy document.',
-		subtitles: [
-			'Our comprehensive travel insurance plans cater to your needs.',
-			'Peace of mind throughout your journey.',
-		],
-	},
-	{
-		scene: 'Split-screen showcasing different coverage benefits.',
-		subtitles: [
-			'From emergency medical expenses to trip cancellation protection.',
-			'Singlife has you covered.',
-		],
-	},
-	{
-		scene: 'The family excitedly boarding a ski lift.',
-		subtitles: [
-			'Enjoy the slopes with confidence.',
-			'Specialized coverage for winter sports activities.',
-		],
-	},
-	{
-		scene: 'A skier performing a jump in mid-air.',
-		subtitles: [
-			'Carve through the powder with peace of mind.',
-			"Don't let unexpected events ruin your ski trip.",
-		],
-	},
-	{
-		scene:
-			'The family gathered around a cozy fireplace in a traditional Japanese lodge.',
-		subtitles: ["Trust Singlife to safeguard your family's adventure."],
-	},
-	{
-		scene: 'Singlife logo and contact information displayed.',
-		subtitles: [
-			'Explore our travel insurance plans on our website.',
-			'Call us today for more information.',
-		],
-	},
-
-	{
-		scene:
-			'The family raising their ski poles in celebration, with a breathtaking mountain backdrop.',
-		subtitles: [
-			'Singlife, protecting your family’s ski adventures.',
-			'Create unforgettable memories together.',
-		],
-	},
-]
 // export const formatted = [
 // 	{
-// 		scene:
-// 			'A family dressed in winter gear, standing in front of a snowy mountain landscape in Japan. They are smiling and holding ski equipment.',
+// 		scene: 'diving in the ocean',
 // 		subtitles: [
 // 			'Are you ready for an unforgettable family ski trip to Japan?',
 // 			"Ensure your adventure is worry-free with Singlife's travel insurance!",
 // 		],
-// 		link: 'https://player.vimeo.com/external/500944403.sd.mp4?s=02f316e3998de2785f9f76a27176723d830b1cee&profile_id=165&oauth2_token_id=57447761',
 // 	},
 // 	{
 // 		scene: 'Close-up of the Singlife travel insurance policy document.',
@@ -72,7 +12,6 @@ export const formatted = [
 // 			'Our comprehensive travel insurance plans cater to your needs.',
 // 			'Peace of mind throughout your journey.',
 // 		],
-// 		link: 'https://player.vimeo.com/external/544012800.hd.mp4?s=2349c4ff6da9f74e9e25057c35a4f17892e0b741&profile_id=174&oauth2_token_id=57447761',
 // 	},
 // 	{
 // 		scene: 'Split-screen showcasing different coverage benefits.',
@@ -80,7 +19,6 @@ export const formatted = [
 // 			'From emergency medical expenses to trip cancellation protection.',
 // 			'Singlife has you covered.',
 // 		],
-// 		link: 'https://player.vimeo.com/external/555732429.sd.mp4?s=c260ab8413a6871b5efc4bcfc5650db0cd31dee2&profile_id=165&oauth2_token_id=57447761',
 // 	},
 // 	{
 // 		scene: 'The family excitedly boarding a ski lift.',
@@ -88,7 +26,6 @@ export const formatted = [
 // 			'Enjoy the slopes with confidence.',
 // 			'Specialized coverage for winter sports activities.',
 // 		],
-// 		link: 'https://player.vimeo.com/external/490498771.hd.mp4?s=5ceab177cc2ae33d1b1e5343c2a675d2503fc85c&profile_id=175&oauth2_token_id=57447761',
 // 	},
 // 	{
 // 		scene: 'A skier performing a jump in mid-air.',
@@ -96,13 +33,11 @@ export const formatted = [
 // 			'Carve through the powder with peace of mind.',
 // 			"Don't let unexpected events ruin your ski trip.",
 // 		],
-// 		link: 'https://player.vimeo.com/external/444929956.hd.mp4?s=0a5b48322c2550ef6a0eb9dd295b32481a1ca8ad&profile_id=170&oauth2_token_id=57447761',
 // 	},
 // 	{
 // 		scene:
 // 			'The family gathered around a cozy fireplace in a traditional Japanese lodge.',
 // 		subtitles: ["Trust Singlife to safeguard your family's adventure."],
-// 		link: ' https://player.vimeo.com/external/505242250.sd.mp4?s=5c69ab3a9fb62658b04f74e9fda3822772cf2c25&profile_id=139&oauth2_token_id=57447761',
 // 	},
 // 	{
 // 		scene: 'Singlife logo and contact information displayed.',
@@ -110,7 +45,6 @@ export const formatted = [
 // 			'Explore our travel insurance plans on our website.',
 // 			'Call us today for more information.',
 // 		],
-// 		link: 'https://player.vimeo.com/external/500944403.sd.mp4?s=02f316e3998de2785f9f76a27176723d830b1cee&profile_id=165&oauth2_token_id=57447761',
 // 	},
 
 // 	{
@@ -120,6 +54,49 @@ export const formatted = [
 // 			'Singlife, protecting your family’s ski adventures.',
 // 			'Create unforgettable memories together.',
 // 		],
-// 		link: ' https://player.vimeo.com/external/505242250.sd.mp4?s=5c69ab3a9fb62658b04f74e9fda3822772cf2c25&profile_id=139&oauth2_token_id=57447761',
 // 	},
 // ]
+export const formatted = [
+	{
+		scene:
+			'A family packing their ski gear with excitement, a map of Japan in the background.',
+		subtitles: [
+			"So, you're off to Japan for a ski trip, huh? Don't forget your toothbrush... or your travel insurance!",
+		],
+	},
+	{
+		scene:
+			'A cartoonish depiction of a person skiing and suddenly falling, with a helicopter coming to the rescue.',
+		subtitles: [
+			"Imagine this: you're skiing, you take a tumble, and bam! You need a helicopter ride. Good thing your insurance covers that, right?",
+		],
+	},
+	{
+		scene:
+			'A person lying in a hospital bed, looking at a calendar with the return date crossed out.',
+		subtitles: [
+			"Oh, and if you're having too much fun (or not) and need to extend your stay, your insurance has got you covered.",
+		],
+	},
+	{
+		scene:
+			'A person looking at their expensive ski gear with a worried expression.',
+		subtitles: [
+			"Brought your fancy gear? Make sure your policy covers that too. You wouldn't want to lose your precious skis, would you?",
+		],
+	},
+	{
+		scene:
+			'A person sneezing into a tissue, with a COVID-19 symbol floating around.',
+		subtitles: [
+			"And let's not forget our uninvited friend, COVID-19. Make sure your policy covers any trip interruptions or medical treatment.",
+		],
+	},
+	{
+		scene:
+			"A person comparing two insurance plans, labeled 'Elite' and 'Classic'.",
+		subtitles: [
+			"Choose wisely, my friend. The 'Elite' for the business mogul, or the 'Classic' for the basics. Either way, stay safe and have fun!",
+		],
+	},
+]
