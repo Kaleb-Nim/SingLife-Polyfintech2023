9423bcb346afd2931e3e88e3d39eea8fa2265476
Soh Hong Yu <sohhongyu@gmail.com>
2023-08-22 23:52:31 +0800

    Update Hong Yu's Changes [FAKE IT TO U MAKE IT]

--- video-gen/src/data.ts
+++ video-gen/src/data.ts
@@ -1,14 +1,45 @@
 // export const formatted = [
 // 	{
-// 		scene:
-// 			'A family packing their ski gear with excitement, a map of Japan in the background.',
+// 		scene: 'diving in the ocean',
 // 		subtitles: [
 // 			"So, you're off to Japan for a ski trip, huh? Don't forget your toothbrush... or your travel insurance!",
 // 		],
 // 	},
 // 	{
+// 		scene: 'Close-up of the Singlife travel insurance policy document.',
+// 		subtitles: [
+// 			'Our comprehensive travel insurance plans cater to your needs.',
+// 			'Peace of mind throughout your journey.',
+// 		],
+// 	},
+// 	{
+// 		scene: 'Split-screen showcasing different coverage benefits.',
+// 		subtitles: [
+// 			'From emergency medical expenses to trip cancellation protection.',
+// 			'Singlife has you covered.',
+// 		],
+// 	},
+// 	{
+// 		scene: 'The family excitedly boarding a ski lift.',
+// 		subtitles: [
+// 			'Enjoy the slopes with confidence.',
+// 			'Specialized coverage for winter sports activities.',
+// 		],
+// 	},
+// 	{
+// 		scene: 'A skier performing a jump in mid-air.',
+// 		subtitles: [
+// 			'Carve through the powder with peace of mind.',
+// 			"Don't let unexpected events ruin your ski trip.",
+// 		],
+// 	},
+// 	{
 // 		scene:
-// 			'A cartoonish depiction of a person skiing and suddenly falling, with a helicopter coming to the rescue.',
+// 			'The family gathered around a cozy fireplace in a traditional Japanese lodge.',
+// 		subtitles: ["Trust Singlife to safeguard your family's adventure."],
+// 	},
+// 	{
+// 		scene: 'Singlife logo and contact information displayed.',
 // 		subtitles: [
 // 			"Imagine this: you're skiing, you take a tumble, and bam! You need a helicopter ride. Good thing your insurance covers that, right?",
 // 		],
@@ -42,6 +73,204 @@
 // 		],
 // 	},
 // ]
+// // export const formatted = [
+// // 	{
+// // 		scene:
+// // 			'A family dressed in winter gear, standing in front of a snowy mountain landscape in Japan. They are smiling and holding ski equipment.',
+// // 		subtitles: [
+// // 			'Are you ready for an unforgettable family ski trip to Japan?',
+// // 			"Ensure your adventure is worry-free with Singlife's travel insurance!",
+// // 		],
+// // 		link: 'https://player.vimeo.com/external/500944403.sd.mp4?s=02f316e3998de2785f9f76a27176723d830b1cee&profile_id=165&oauth2_token_id=57447761',
+// // 	},
+// // 	{
+// // 		scene: 'Close-up of the Singlife travel insurance policy document.',
+// // 		subtitles: [
+// // 			'Our comprehensive travel insurance plans cater to your needs.',
+// // 			'Peace of mind throughout your journey.',
+// // 		],
+// // 		link: 'https://player.vimeo.com/external/544012800.hd.mp4?s=2349c4ff6da9f74e9e25057c35a4f17892e0b741&profile_id=174&oauth2_token_id=57447761',
+// // 	},
+// // 	{
+// // 		scene: 'Split-screen showcasing different coverage benefits.',
+// // 		subtitles: [
+// // 			'From emergency medical expenses to trip cancellation protection.',
+// // 			'Singlife has you covered.',
+// // 		],
+// // 		link: 'https://player.vimeo.com/external/555732429.sd.mp4?s=c260ab8413a6871b5efc4bcfc5650db0cd31dee2&profile_id=165&oauth2_token_id=57447761',
+// // 	},
+// // 	{
+// // 		scene: 'The family excitedly boarding a ski lift.',
+// // 		subtitles: [
+// // 			'Enjoy the slopes with confidence.',
+// // 			'Specialized coverage for winter sports activities.',
+// // 		],
+// // 		link: 'https://player.vimeo.com/external/490498771.hd.mp4?s=5ceab177cc2ae33d1b1e5343c2a675d2503fc85c&profile_id=175&oauth2_token_id=57447761',
+// // 	},
+// // 	{
+// // 		scene: 'A skier performing a jump in mid-air.',
+// // 		subtitles: [
+// // 			'Carve through the powder with peace of mind.',
+// // 			"Don't let unexpected events ruin your ski trip.",
+// // 		],
+// // 		link: 'https://player.vimeo.com/external/444929956.hd.mp4?s=0a5b48322c2550ef6a0eb9dd295b32481a1ca8ad&profile_id=170&oauth2_token_id=57447761',
+// // 	},
+// // 	{
+// // 		scene:
+// // 			'The family gathered around a cozy fireplace in a traditional Japanese lodge.',
+// // 		subtitles: ["Trust Singlife to safeguard your family's adventure."],
+// // 		link: ' https://player.vimeo.com/external/505242250.sd.mp4?s=5c69ab3a9fb62658b04f74e9fda3822772cf2c25&profile_id=139&oauth2_token_id=57447761',
+// // 	},
+// // 	{
+// // 		scene: 'Singlife logo and contact information displayed.',
+// // 		subtitles: [
+// // 			'Explore our travel insurance plans on our website.',
+// // 			'Call us today for more information.',
+// // 		],
+// // 		link: 'https://player.vimeo.com/external/500944403.sd.mp4?s=02f316e3998de2785f9f76a27176723d830b1cee&profile_id=165&oauth2_token_id=57447761',
+// // 	},
+
+// // 	{
+// // 		scene:
+// // 			'The family raising their ski poles in celebration, with a breathtaking mountain backdrop.',
+// // 		subtitles: [
+// // 			'Singlife, protecting your family’s ski adventures.',
+// // 			'Create unforgettable memories together.',
+// // 		],
+// // 		link: ' https://player.vimeo.com/external/505242250.sd.mp4?s=5c69ab3a9fb62658b04f74e9fda3822772cf2c25&profile_id=139&oauth2_token_id=57447761',
+// // 	},
+// // ]
+
+// export const formatted = [
+// 	{
+// 		scene: 'ski montagne skier piste de ski',
+// 		subtitles: [
+// 			"So, you're off to Japan for a ski trip, huh?",
+// 			"Don't forget your toothbrush...",
+// 			'or your travel insurance!',
+// 		],
+// 	},
+// 	{
+// 		scene: 'a man skiing on an icy mountain',
+// 		subtitles: [
+// 			"Imagine this: you're skiing, you take a tumble, and ",
+// 			'BAM!',
+// 			'You need a helicopter ride.',
+// 			'Good thing your insurance covers that, right?',
+// 		],
+// 	},
+// 	{
+// 		scene: 'man doctor health hospital sleeping',
+// 		subtitles: [
+// 			"Oh, and if you're having too much fun (or not)",
+// 			'and need to extend your stay, ',
+// 			'your insurance has got you covered.',
+// 		],
+// 	},
+// 	{
+// 		scene:
+// 			'A person looking at their expensive ski gear with a worried expression.',
+// 		subtitles: [
+// 			'Brought your fancy gear? Make sure your policy covers that too.',
+// 			"You wouldn't want to lose your precious skis, would you?",
+// 		],
+// 	},
+// 	{
+// 		scene:
+// 			'A person sneezing into a tissue, with a COVID-19 symbol floating around.',
+// 		subtitles: [
+// 			"And let's not forget our uninvited friend, COVID-19.",
+// 			'Make sure your policy covers any trip interruptions or medical treatment.',
+// 		],
+// 	},
+// 	{
+// 		scene:
+// 			"A person comparing two insurance plans, labeled 'Elite' and 'Classic'.",
+// 		subtitles: [
+// 			'Choose wisely, my friend.',
+// 			"The 'Elite' for the business mogul,",
+// 			"or the 'Classic' for the basics.",
+// 			'Either way, stay safe and have fun!',
+// 		],
+// 	},
+// ]
+
+// export const formatted = [
+// 	{
+// 		scene: 'calculating and taking notes vertical',
+// 		subtitles: [
+// 			"So, you're planning for your future, huh?",
+// 			'Getting that S$1.5 million payout at 60 is an excellent goal!',
+// 			'But how can you make it happen?',
+// 		],
+// 	},
+// 	{
+// 		scene:
+// 			'A person browsing a financial planning website on a laptop, with a cup of coffee beside them. vertical',
+// 		subtitles: [
+// 			'Imagine this:',
+// 			'you find the perfect financial plan that helps you achieve your dream payout.',
+// 			"It's like a tailored suit, but for your retirement!",
+// 		],
+// 	},
+// 	{
+// 		scene:
+// 			'A person talking to a friendly financial advisor, with a whiteboard filled with investment options in the background.',
+// 		subtitles: [
+// 			"And if you're unsure about where to start,",
+// 			"Don't Worry!",
+// 			'A reliable financial advisor can guide you towards the best investment strategies.',
+// 		],
+// 	},
+// 	{
+// 		scene: 'woman blowing out candles old with family and friends',
+// 		subtitles: [
+// 			'Picture this:',
+// 			"you're 60 years old, surrounded by loved ones,",
+// 			'celebrating your success.',
+// 			"You've reached your S$1.5 million payout!",
+// 		],
+// 	},
+// 	{
+// 		scene: 'people doing warrior pose in yoga class group',
+// 		subtitles: [
+// 			'But Wait!',
+// 			"Achieving your financial goals isn't just about investments.",
+// 			'Taking care of your health is equally important!',
+// 			'A healthy lifestyle ensures a brighter future.',
+// 		],
+// 	},
+// 	{
+// 		scene:
+// 			'family looking for house vertical boy Woman Showing a House to Clients',
+// 		subtitles: [
+// 			'Explore different investment paths, like stocks, real estate, or retirement funds.',
+// 			'Diversifying your portfolio increases your chances of hitting that S$1.5 million mark!',
+// 		],
+// 	},
+// 	{
+// 		scene: 'A Person Planning and Looking at World Map',
+// 		subtitles: [
+// 			'And when you reach your goal,',
+// 			'the world is your oyster!',
+// 			'Plan for an enjoyable retirement filled with adventures and new experiences.',
+// 		],
+// 	},
+// 	{
+// 		scene: 'Man Enjoying his Day on Hammock beach vertical blue',
+// 		subtitles: [
+// 			'Imagine this:',
+// 			'sipping coconut water, feeling the warm sun on your skin, and living your best life in retirement.',
+// 			"It's all possible with the right financial planning!",
+// 		],
+// 	},
+// ]
+
+// var string = ''
+// for (let subtitle of formatted) {
+// 	string += subtitle['subtitles'][0] + ' '
+// }
+// console.log(string)
 
 export const formatted = [
 	{
